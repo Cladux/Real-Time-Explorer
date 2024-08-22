8f462096745f840aa8d018f9e60c8a7886dcbd9b
Soroush Sharifi <sharifysoroush@gmail.com>
2024-08-22 22:28:11 +0330

    dev: main page, get cities & bg image API. build: react query, zustand, axios, react icons. comp: Background, Nav, ProfileDropdown.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,29 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  env: {
+    API_NINJA: "Tslhr/CBrX41krGc1gKKvA==os3kleuQIXFpWliA",
+    UNSPLASH_ACCESS_KEY: "f5WTMLK7AMH_OYHS5UHXRgRO9dqwil1fFA-82vswbjU",
+  },
   experimental: {
     optimizePackageImports: ["zod", "sharp", "next-auth", "framer-motion", "@nextui-org/react"],
     serverActions: {
       bodySizeLimit: "2mb",
     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
   poweredByHeader: false,
 };
